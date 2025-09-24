841dadfa947946be632d1eff9704493505465e46
CodeMahmoud <mahmoudcode913@gmail.com>
2025-09-24 13:32:55 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mak on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Mak on 9/24/25.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
