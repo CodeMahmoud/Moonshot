002d98b78730a2a163eea595fe7342dd13f94be9
CodeMahmoud <mahmoudcode913@gmail.com>
2025-09-27 16:39:29 -0500

    Initial commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Mak on 9/24/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
